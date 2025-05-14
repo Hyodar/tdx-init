61f7dc1641e60f2c351c2e331f65d49ebeb66992
alexhulbert <alex@alexhulbert.com>
2025-05-14 15:06:15 -0400

    Create and set perms on /persistent subfolders

--- passphrase.go
+++ passphrase.go
@@ -30,13 +30,16 @@ func setPassphrase() {
 
 	if isNewSetup {
 		setupNewDisk(passphrase)
+		setupMountDirs()
 	} else {
 		mountExistingDisk(passphrase)
 	}
+
 }
 
 func setupNewDisk(passphrase string) {
 	// Format with LUKS2
+	log.Println("Formatting disk with LUKS2...")
 	cmd := exec.Command("cryptsetup", "luksFormat", "--type", "luks2", "-q", devicePath)
 	cmd.Stdin = strings.NewReader(passphrase)
 	if err := cmd.Run(); err != nil {
@@ -51,6 +54,7 @@ func setupNewDisk(passphrase string) {
 	}
 
 	// Create ext4 filesystem
+	log.Println("Creating ext4 filesystem...")
 	if err := exec.Command("mkfs.ext4", mapperDevice).Run(); err != nil {
 		exec.Command("cryptsetup", "close", mapperName).Run()
 		log.Fatalf("Error creating filesystem: %v\n", err)
@@ -84,6 +88,8 @@ func setupNewDisk(passphrase string) {
 		log.Fatalf("Error marshaling token JSON: %v", err)
 	}
 
+	// Import the token into the LUKS header
+	log.Println("Saving searcher SSH key...")
 	cmd = exec.Command("cryptsetup", "token", "import", "--token-id", "1", devicePath)
 	cmd.Stdin = strings.NewReader(string(tokenJSON))
 
@@ -111,6 +117,26 @@ func mountExistingDisk(passphrase string) {
 	}
 
 	fmt.Println("Encrypted disk mounted successfully")
+}
+
+func setupMountDirs() {
+	dirs := []string{"searcher", "delayed_logs", "searcher_logs"}
+	for _, dir := range dirs {
+		path := fmt.Sprintf("%s/%s", mountPoint, dir)
+		if err := os.MkdirAll(path, 0755); err != nil {
+			log.Fatalf("Error creating directory %s: %v\n", path, err)
+		}
+	}
+
+	if err := os.Chown(fmt.Sprintf("%s/searcher", mountPoint), 1000, 1000); err != nil {
+		log.Fatalf("Error setting ownership for searcher: %v\n", err)
+	}
+	if err := os.Chown(fmt.Sprintf("%s/searcher_logs", mountPoint), 1000, 1000); err != nil {
+		log.Fatalf("Error setting ownership for searcher_logs: %v\n", err)
+	}
+	if err := os.Chmod(fmt.Sprintf("%s/searcher_logs", mountPoint), 0755); err != nil {
+		log.Fatalf("Error setting permissions for searcher_logs: %v\n", err)
+	}
 }
 
 func checkMounted() bool {
